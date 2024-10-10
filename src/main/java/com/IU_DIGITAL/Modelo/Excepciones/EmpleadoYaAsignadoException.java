d0c4cc6899f7470f2e0b7c4da743603bab7b0422
Daniel-999-est <daniel.asprilla@est.iudigital.edu.co>
2024-10-10 00:47:20 -0500

    Agregar excepción para empleados ya asignados

--- src/main/java/com/IU_DIGITAL/Modelo/Excepciones/EmpleadoYaAsignadoException.java
+++ src/main/java/com/IU_DIGITAL/Modelo/Excepciones/EmpleadoYaAsignadoException.java
@@ -1,4 +1,10 @@
 package com.IU_DIGITAL.Modelo.Excepciones;
 
-public class EmpleadoYaAsignadoException {
+
+// Clase de excepción personalizada para indicar que un empleado ya ha sido asignado
+public class EmpleadoYaAsignadoException extends Exception {
+    // Constructor que recibe un mensaje de error
+    public EmpleadoYaAsignadoException(String mensaje) {
+        super(mensaje); // Llama al constructor de la clase padre (Exception)
+    }
 }
