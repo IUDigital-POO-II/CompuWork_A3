335675178bbeff0f3a311f60d6741c7fe8372246
Jair Moreno <jairmorenolml@gmail.com>
2024-10-10 08:33:20 -0500

    Subiendo Clase Main

--- src/main/java/com/IU_DIGITAL/Main.java
+++ src/main/java/com/IU_DIGITAL/Main.java
@@ -1,4 +1,20 @@
 package com.IU_DIGITAL;
 
+import com.IU_DIGITAL.Vista.VentanaPrincipal;
+
+import javax.swing.SwingUtilities;
+
 public class Main {
+    public static void main(String[] args) {
+        // Usamos SwingUtilities.invokeLater para asegurarnos de que la creacion y manipulacion
+        // de componentes de la interfaz grafica se realice en el hilo de despacho de eventos (EDT).
+        SwingUtilities.invokeLater(() -> {
+
+            // Crear una instancia de la ventana principal de la aplicacion
+            VentanaPrincipal app = new VentanaPrincipal();
+
+            // Hacer visible la ventana principal
+            app.setVisible(true);
+        });
+    }
 }
