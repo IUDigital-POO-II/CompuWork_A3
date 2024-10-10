b62b4baf11e93ce46e20b44d83104895bdbd9885
Daniel-999-est <daniel.asprilla@est.iudigital.edu.co>
2024-10-10 00:50:23 -0500

    Agregar excepción para Reportes no generado

--- src/main/java/com/IU_DIGITAL/Modelo/Excepciones/ReporteNoGeneradoException.java
+++ src/main/java/com/IU_DIGITAL/Modelo/Excepciones/ReporteNoGeneradoException.java
@@ -1,4 +1,10 @@
 package com.IU_DIGITAL.Modelo.Excepciones;
 
-public class ReporteNoGeneradoException {
+
+// Clase de excepción personalizada para indicar que un reporte no pudo ser generado
+public class ReporteNoGeneradoException extends Exception {
+    // Constructor que recibe un mensaje de error
+    public ReporteNoGeneradoException(String mensaje) {
+        super(mensaje); // Llama al constructor de la clase padre (Exception)
+    }
 }
